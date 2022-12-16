3c94e679133294e7fae1c874e86cf3eea023331a
Luka Dornhecker <luka.dornhecker@hey.com>
2022-12-17 00:41:46 +0100

    solve 2022 16 part 2

--- solutions/src/year_2022/day16.rs
+++ solutions/src/year_2022/day16.rs
@@ -22,19 +22,35 @@ struct Valve<'a> {
 #[date(year = 2022, day = 16)]
 pub struct Day16;
 
+#[allow(clippy::too_many_arguments)]
 fn max_flow(
+    start: u64,
     current: u64,
     opened: u64,
     flows: &HashMap<u64, u64>,
     tunnels: &HashMap<u64, Vec<u64>>,
-    mins_left: u64,
-    cache: &mut HashMap<(u64, u64, u64), u64>,
+    mins_left: u8,
+    elephants: u8,
+    cache: &mut HashMap<(u64, u64, u8, u8), u64>,
 ) -> u64 {
     if mins_left == 0 {
-        return 0;
+        return if elephants > 0 {
+            max_flow(
+                start,
+                start,
+                opened,
+                flows,
+                tunnels,
+                26,
+                elephants - 1,
+                cache,
+            )
+        } else {
+            0
+        };
     }
 
-    let key = (current, opened, mins_left);
+    let key = (current, opened, mins_left, elephants);
     if let Some(value) = cache.get(&key) {
         return *value;
     }
@@ -42,13 +58,22 @@ fn max_flow(
     let mut pressure = 0;
 
     if opened & current == 0 {
-        let val = (mins_left - 1) * flows.get(&current).unwrap();
+        let val = (mins_left as u64 - 1) * flows.get(&current).unwrap();
 
         if val != 0 {
             let cur_opened = opened | current;
             for &neighbor in tunnels.get(&current).unwrap() {
                 pressure = pressure.max(
-                    val + max_flow(neighbor, cur_opened, flows, tunnels, mins_left - 2, cache),
+                    val + max_flow(
+                        start,
+                        neighbor,
+                        cur_opened,
+                        flows,
+                        tunnels,
+                        mins_left - 2,
+                        elephants,
+                        cache,
+                    ),
                 );
             }
         }
@@ -56,11 +81,13 @@ fn max_flow(
 
     for &neighbor in tunnels.get(&current).unwrap() {
         pressure = pressure.max(max_flow(
+            start,
             neighbor,
             opened,
             flows,
             tunnels,
             mins_left - 1,
+            elephants,
             cache,
         ));
     }
@@ -68,6 +95,8 @@ fn max_flow(
     cache.insert(key, pressure);
     pressure
 }
+
+// fn solve(input: &str, mins_left: u64);
 
 impl Solution for Day16 {
     fn part_one(&self, input: &str) -> AocResult {
@@ -98,10 +127,52 @@ impl Solution for Day16 {
 
         Ok(Box::new(max_flow(
             *ids.get(&370).unwrap(),
+            *ids.get(&370).unwrap(),
             0,
             &flows,
             &tunnels,
             30,
+            0,
+            &mut cache,
+        )))
+    }
+
+    fn part_two(&self, input: &str) -> AocResult {
+        let mut tunnels = HashMap::new();
+        let mut flows = HashMap::new();
+        let mut ids = HashMap::new();
+
+        let mut valves: Vec<_> = iterator(input, parse_line).into_iter().collect();
+
+        for (i, valve) in valves.iter_mut().enumerate() {
+            let new_id = 1 << i;
+            ids.insert(valve.id, new_id);
+            valve.id = new_id;
+        }
+
+        dbg!(&ids);
+
+        for valve in valves.iter_mut() {
+            for tunnel in valve.tunnels.iter_mut() {
+                *tunnel = *ids.get(tunnel).unwrap();
+            }
+        }
+
+        for valve in valves.into_iter() {
+            flows.insert(valve.id, valve.rate);
+            tunnels.insert(valve.id, valve.tunnels);
+        }
+
+        let mut cache = HashMap::new();
+
+        Ok(Box::new(max_flow(
+            *ids.get(&370).unwrap(),
+            *ids.get(&370).unwrap(),
+            0,
+            &flows,
+            &tunnels,
+            26,
+            1,
             &mut cache,
         )))
     }
@@ -145,4 +216,5 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II
 ";
     assert_solution!(Day16.part_one, input, "1651");
+    assert_solution!(Day16.part_two, input, "1707");
 }
